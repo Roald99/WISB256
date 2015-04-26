0b7f2f8df30debf189133011b72da4ee2826e917
Roald99 <mailvanroaldneuteboom@hotmail.com>
2015-04-26 14:02:07 +0200

    Update ManyGreetings.py
    wijzigingen aangebracht, "What is your name?" ipv "Name?" en spaties aan het einde van de twee regels toegevoegd.

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,4 +1,4 @@
-name= input('Name?')
+name= input('What is your name? ')
 #print('Hello, ' + name + '!')
-amountofgreetings = input('How many greetings?')
+amountofgreetings = input('How many greetings? ')
 print(amountofgreetings + ' x Hello, ' + name + '!')
